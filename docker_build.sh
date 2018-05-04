a80645c3701e4bbb1beeba21e3bc64ed60c1df75
Nick Miller <nick.miller@onyxpoint.com>
2018-05-03 23:51:10 -0400

    set -e

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 docker build -t ruby-2.5.1-alpine-arm https://raw.githubusercontent.com/docker-library/ruby/c9644fe5c95cd71913db348baa41240f05d882b3/2.5/alpine3.7/Dockerfile
 docker tag ruby-2.5.1-alpine-arm us.gcr.io/jeefme-185614/ruby:2.5.1-alpine
 
