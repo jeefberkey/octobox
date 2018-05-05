3bb7f090bf8a03d9a6d7d76929166ccc816c938c
Nick Miller <nick.miller@onyxpoint.com>
2018-05-05 16:35:53 -0400

    no cache

--- docker_build.sh
+++ docker_build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-docker build --network=host -t ruby-2.5.1-alpine-arm https://raw.githubusercontent.com/docker-library/ruby/c9644fe5c95cd71913db348baa41240f05d882b3/2.5/alpine3.7/Dockerfile
+docker build --no-cache --network=host -t ruby-2.5.1-alpine-arm https://raw.githubusercontent.com/docker-library/ruby/c9644fe5c95cd71913db348baa41240f05d882b3/2.5/alpine3.7/Dockerfile
 docker tag ruby-2.5.1-alpine-arm us.gcr.io/jeefme-185614/ruby:2.5.1-alpine
 
-docker build --network=host -t octobox-arm .
+docker build --no-cache --network=host -t octobox-arm .
 docker tag octobox-arm us.gcr.io/jeefme-185614/octobox:latest
 
