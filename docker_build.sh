4e6e88ab14faea8877f2056e6377d705a3ae4f25
Nick Miller <nick.miller@onyxpoint.com>
2018-08-26 18:15:18 -0400

    images built on ci should be tagged latest-ci

--- docker_build.sh
+++ docker_build.sh
@@ -8,4 +8,8 @@ docker tag ruby-2.5.1-alpine-arm us.gcr.io/jeefme-185614/ruby:2.5.1-alpine
 
 docker build --network=host -t octobox-arm .
 docker tag octobox-arm us.gcr.io/jeefme-185614/octobox:`git rev-parse --short HEAD`
+docker tag octobox-arm us.gcr.io/jeefme-185614/octobox:latest
+
+docker push us.gcr.io/jeefme-185614/octobox:`git rev-parse --short head`
+docker push us.gcr.io/jeefme-185614/octobox:latest
 
