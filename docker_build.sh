b408989140045400fc49752bc010ce8e187c9560
Nick Miller <nick.miller@onyxpoint.com>
2018-09-13 18:10:18 -0400

    caps HEAD

--- docker_build.sh
+++ docker_build.sh
@@ -10,6 +10,6 @@ docker build --network=host -t octobox-arm .
 docker tag octobox-arm us.gcr.io/jeefme-185614/octobox:`git rev-parse --short HEAD`
 docker tag octobox-arm us.gcr.io/jeefme-185614/octobox:latest
 
-docker push us.gcr.io/jeefme-185614/octobox:`git rev-parse --short head`
+docker push us.gcr.io/jeefme-185614/octobox:`git rev-parse --short HEAD`
 docker push us.gcr.io/jeefme-185614/octobox:latest
 
