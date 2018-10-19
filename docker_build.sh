1a1d82d9556bb06fda0e4b1a383e0c4c39abf29e
Nick Miller <nick.miller@onyxpoint.com>
2018-10-19 15:29:49 -0400

    update to ruby 2.5.3

--- docker_build.sh
+++ docker_build.sh
@@ -3,8 +3,8 @@
 
 set -e
 
-docker build --network=host -t ruby-2.5.1-alpine-arm https://raw.githubusercontent.com/docker-library/ruby/ccf00a6b31abe14d27bdda498707a2ce338ef019/2.5/alpine3.7/Dockerfile
-docker tag ruby-2.5.1-alpine-arm us.gcr.io/jeefme-185614/ruby:2.5.1-alpine
+docker build --network=host -t ruby-2.5.3-alpine-arm https://raw.githubusercontent.com/docker-library/ruby/0bdbbff7475155c90a5945f13daf7067a7bd9f11/2.5/alpine3.7/Dockerfile
+docker tag ruby-2.5.3-alpine-arm us.gcr.io/jeefme-185614/ruby:2.5.3-alpine
 
 docker build --network=host -t octobox-arm .
 docker tag octobox-arm us.gcr.io/jeefme-185614/octobox:`git rev-parse --short HEAD`
