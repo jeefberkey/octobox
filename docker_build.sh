2a4e5ab46b9a54e18fc1c64d31c31cb74829bb87
Nick Miller <nick.miller@onyxpoint.com>
2019-01-07 22:10:09 -0500

    ruby 2.6.0

--- docker_build.sh
+++ docker_build.sh
@@ -4,7 +4,7 @@
 set -e
 
 docker build --network=host -t ruby-2.6.0-alpine-arm https://raw.githubusercontent.com/docker-library/ruby/84db4691c080384c8fbce44c722d46cedd6a384b/2.6/alpine3.8/Dockerfile
-docker tag ruby-2.5.3-alpine-arm us.gcr.io/jeefme-185614/ruby:2.6.0-alpine
+docker tag ruby-2.6.0-alpine-arm us.gcr.io/jeefme-185614/ruby:2.6.0-alpine
 
 docker build --network=host -t octobox-arm .
 docker tag octobox-arm us.gcr.io/jeefme-185614/octobox:`git rev-parse --short HEAD`
