ec0cc01aa751a4db544409098a2a9afa6f1783da
Nick Miller <nick.miller@onyxpoint.com>
2019-03-02 22:58:24 -0500

    2.6.1 has an arm alpine image

--- docker_build.sh
+++ docker_build.sh
@@ -3,8 +3,8 @@
 
 set -e
 
-docker build --network=host -t ruby-2.6.0-alpine-arm https://raw.githubusercontent.com/docker-library/ruby/84db4691c080384c8fbce44c722d46cedd6a384b/2.6/alpine3.8/Dockerfile
-docker tag ruby-2.6.0-alpine-arm us.gcr.io/jeefme-185614/ruby:2.6.0-alpine
+#docker build --network=host -t ruby-2.6.1-alpine-arm https://raw.githubusercontent.com/docker-library/ruby/eae22dc2dfe30b48f1633912396110539e1b8d49/2.6/alpine3.9/Dockerfile
+#docker tag ruby-2.6.1-alpine-arm us.gcr.io/jeefme-185614/ruby:2.6.1-alpine
 
 docker build --network=host -t octobox-arm .
 docker tag octobox-arm us.gcr.io/jeefme-185614/octobox:`git rev-parse --short HEAD`
